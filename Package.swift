cf6411d307dda68bd63eeb726167bb00e33645e4
Steven Clukey <saclukey@live.com>
2016-09-03 12:35:13 -0400

    Add dependence on GTK on Linux
    On Linux, we need GTK+3, I created a swift wrapper for that and add the
    dependency here.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
+#if os(Linux)
+let dependencies:[Package.Dependency] = [.Package(url: "../cgtk", majorVersion: 3)]
+#else
+let dependencies:[Package.Dependency] = []
+#endif
+
 let package = Package(
-    name: "clibui"
+    name: "clibui",
+    dependencies: dependencies
 )
