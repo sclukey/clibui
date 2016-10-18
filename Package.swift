1f283617c12b1e976d3440ab264deafe4a2a680d
Steven Clukey <saclukey@live.com>
2016-10-17 23:11:53 -0400

    Use custom pkgConfig files for linking
    This requires an as of yet unaccepted pull request in SwiftPM, but it
    will make using this library dramatically easier.

--- Package.swift
+++ Package.swift
@@ -2,11 +2,14 @@ import PackageDescription
 
 #if os(Linux)
 let dependencies:[Package.Dependency] = [.Package(url: "https://github.com/sclukey/cgtk3.git", majorVersion: 1)]
+let pcFile = MemoryLayout<Int>.size == 4 ? "ui-linux_386" : "ui-linux_amd64"
 #else
 let dependencies:[Package.Dependency] = []
+let pcFile = "ui-darwin_amd64"
 #endif
 
 let package = Package(
     name: "clibui",
+    pkgConfig: pcFile,
     dependencies: dependencies
 )
